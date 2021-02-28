fda539df06cc23bcb4bc2d5b80e6a53b7e51c606
zkh <1019003797@qq.com>
2021-02-28 23:59:53 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,12 @@
+"""
+print("hello world")
+print(True)   # bool
+print(233)
+print("haha",2333)  
+print("hh"*100) 
+print((99+100)-32)
+
+"""
+a = (input("请输入"))
+b=(input("请输入"))
+print("数据的长度之和",len(a)+len(b))
